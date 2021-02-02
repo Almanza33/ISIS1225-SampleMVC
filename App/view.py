a3555f8646c1e144bc2057b5e0c346f7d5a061d0
Almanza33 <d.almanza@uniandes.edu.co>
2021-02-01 19:32:28 -0500

    cambio nombres y To -do completado

--- App/view.py
+++ App/view.py
@@ -43,6 +43,7 @@ def printMenu():
     print("1- Cargar Libros")
     print("2- Cargar Tags")
     # TO-DO: Modificaciones para completar el laboratorio 1.
+    print("3- Cargar Book-tags")
     print("0- Salir")
 
 
@@ -76,7 +77,9 @@ while True:
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
     # TO-DO: Modificaciones para completar el laboratorio 1.
-
+    elif int(inputs[0]) == 3:
+        print("Cargando información de book-tags....")
+        print('Total de tags cargados: ')
     else:
         sys.exit(0)
 sys.exit(0)
